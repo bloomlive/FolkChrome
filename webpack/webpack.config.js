77927f4de19a42e201deed4cf6357378a6b2b2d6
Daniel Sepp <43651556+bloomlive@users.noreply.github.com>
2023-07-01 20:58:11 +0300

    Change formatting

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,34 +1,38 @@
-const path = require('path');
-const CopyPlugin = require('copy-webpack-plugin');
-const {CleanWebpackPlugin} = require("clean-webpack-plugin");
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
-    mode: "production",
-    entry: {
-        background: path.resolve(__dirname, "..", "src", "background.ts"),
-    },
-    output: {
-        path: path.join(__dirname, "../dist"),
-        filename: "[name].js",
-    },
-    resolve: {
-        extensions: [".ts", ".js"],
-    },
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                loader: "ts-loader",
-                exclude: /node_modules/,
-            },
-        ],
-    },
-    plugins: [
-        new CleanWebpackPlugin({}),
-        new CopyPlugin({
-            patterns: [{
-                from: ".", to: ".", context: "public"
-            }]
-        }),
+  mode: "production",
+  entry: {
+    background: path.resolve(__dirname, "..", "src", "background.ts"),
+  },
+  output: {
+    path: path.join(__dirname, "../dist"),
+    filename: "[name].js",
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: "ts-loader",
+        exclude: /node_modules/,
+      },
     ],
+  },
+  plugins: [
+    new CleanWebpackPlugin({}),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: ".",
+          to: ".",
+          context: "public",
+        },
+      ],
+    }),
+  ],
 };
